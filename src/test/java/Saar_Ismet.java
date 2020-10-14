336606a254dca2370c268e589fb10a1edc35847d
IsmetYigitbasi <ismetyigitbasi@gmail.com>
2020-10-14 17:20:49 +0200

    Added Saar Class

---
+++ src/test/java/Saar_Ismet.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+import java.util.Scanner;
+
+public class Saar_Ismet {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter a city: ");
+        String city = scan.nextLine();
+        System.out.println("city = " + city);
+
+    }
+
+
+
+
+}
