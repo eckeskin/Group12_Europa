b44476ad8cc2ffbb525663fcb62d885ff2cef25f
IsmetYigitbasi <ismetyigitbasi@gmail.com>
2020-10-14 17:46:24 +0200

    Updated Saar Class

--- src/test/java/Saar_Ismet.java
+++ src/test/java/Saar_Ismet.java
@@ -7,8 +7,11 @@ public class Saar_Ismet {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Please enter a city: ");
+        System.out.print("Please enter your city: ");
         String city = scan.nextLine();
+        // added line 12 and line 13
+        System.out.print("Please enter your town: ");
+        String town = scan.nextLine();
         System.out.println("city = " + city);
         System.out.println("town = " + town);
         
